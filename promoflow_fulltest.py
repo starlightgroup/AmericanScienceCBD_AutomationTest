69e4a4dd71ae4644dd78bbe198b7ed0775ac47b5
Vikas Neha Ojha <neha.nehavikas.vikas@gmail.com>
2018-07-09 18:47:52 +0530

    Code Refactor and Checkout Page Test Added

--- promoflow_fulltest.py
+++ promoflow_fulltest.py
@@ -22,6 +22,11 @@ class PromoFlowTest(unittest.TestCase):
         cls.driver.set_page_load_timeout(30)
 
     def test_promo_flow(self):
+        self.promo_flow_test1_promo_page_load()
+        self.promo_flow_test2_shipping_page_submit()
+        self.promo_flow_test3_checkout_page_submit()
+
+    def promo_flow_test1_promo_page_load(self):
         # TEST 1: test to check if promo page loads correctly
         url = 'https://americansciencecbd.com/promo'
         self.driver.get(url)
@@ -31,6 +36,7 @@ class PromoFlowTest(unittest.TestCase):
         self.assertTrue(self.is_element_present(By.NAME, 'FirstName', check_for_visibility=True))
         self.assertTrue(self.is_element_present(By.XPATH, '//button[@id="rush-my-order-form-click"]', True))
 
+    def promo_flow_test2_shipping_page_submit(self):
         # TEST 2: Shipping Page Submit
         self.driver.find_element_by_name('FirstName').send_keys('fname')
         self.driver.find_element_by_name('LastName').send_keys('lname')
@@ -42,10 +48,30 @@ class PromoFlowTest(unittest.TestCase):
         self.driver.find_element_by_name('Phone').send_keys('9999911111')
         self.driver.find_element_by_name('Email').send_keys('abc@abc.com')
         self.driver.find_element_by_xpath('//button[@id="rush-my-order-form-click"]').click()
-        sleep(1)
+        sleep(5)
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, 'cardNumber')))
         self.assertEqual(self.driver.current_url, 'https://americansciencecbd.com/promo/desktop/checkout?',
                          'Checkout Page URL is wrong - {}'.format(self.driver.current_url))
+
+    def promo_flow_test3_checkout_page_submit(self):
+        # TEST 3: Checkout Page Submit
+        self.assertTrue(self.is_element_present(By.ID, 'select-package-210', check_for_visibility=True))
+        self.assertTrue(self.is_element_present(By.ID, 'select-package-209', check_for_visibility=True))
+        self.assertTrue(self.is_element_present(By.ID, 'select-package-208', check_for_visibility=True))
+        self.assertTrue(self.is_element_present(By.NAME, 'cardNumber', check_for_visibility=True))
+        self.assertTrue(self.is_element_present(By.NAME, 'cardMonth', check_for_visibility=True))
+        self.assertTrue(self.is_element_present(By.NAME, 'cardYear', check_for_visibility=True))
+        self.assertTrue(self.is_element_present(By.NAME, 'cardSecurityCode', check_for_visibility=True))
+
+        self.driver.find_element_by_id('select-package-210').click()
+        self.driver.find_element_by_name('cardNumber').send_keys('1333333333333333')
+        Select(self.driver.find_element_by_name('cardMonth')).select_by_visible_text('12')
+        Select(self.driver.find_element_by_name('cardYear')).select_by_visible_text('22')
+        self.driver.find_element_by_name('cardSecurityCode').send_keys('465')
+        self.driver.find_element_by_id('checkout-submit-desktop').click()
+        sleep(4)
+        self.assertEqual(self.driver.current_url, 'https://americansciencecbd.com/promo/desktop/upsell-1?',
+                         'Upsell1 Page URL is wrong - {}'.format(self.driver.current_url))
 
     @classmethod
     def tearDownClass(cls):
@@ -74,5 +100,5 @@ if __name__ == '__main__':
     runner = unittest.TextTestRunner(f)
     unittest.main(testRunner=runner, exit=False)
     f.close()
-    with open(outfile, 'r') as f:
-        post_message_slack(f.read())
+    # with open(outfile, 'r') as f:
+    #     post_message_slack(f.read())
